8ebd5f69d17d57e00e7d9d304329f99f836eb7d1
Gabriel Silva Cangussu <115298770+Biel2909@users.noreply.github.com>
2026-03-11 16:27:22 -0300

    Delete ProjetoC directory

--- ProjetoC/script.js
+++
@@ -1,9 +0,0 @@
-function corte(){
-    window.location.href = "HTML/cortes.html";
-}
-function agendamento(){
-    window.location.href="HTML/agendamento.html";
-}
-function servico(){
-    window.location.href = "HTML/servicos.html";
-}
